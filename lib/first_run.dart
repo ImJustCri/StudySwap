aa4d325965474727af0fdde8939087584e0ed50f
ImJustCri <cricirsicily@proton.me>
2025-06-19 13:46:09 +0200

    Add authentication check on app launch

--- lib/first_run.dart
+++ lib/first_run.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:is_first_run/is_first_run.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:studyswap/misc/resources.dart';
 
 class LandingPage extends StatelessWidget {
@@ -7,17 +8,25 @@ class LandingPage extends StatelessWidget {
 
   Future<bool> _checkFirstRun() => IsFirstRun.isFirstRun();
 
-  void _reset() async {
-    await IsFirstRun.reset();
-    _checkFirstRun();
+  Future<String> _getInitialRoute() async {
+    // Check first run
+    final isFirstRun = await _checkFirstRun();
+    if (isFirstRun) return '/onboarding';
+
+    // Check authentication
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return '/login';
+
+    // If authenticated, go to home
+    return '/homescreen';
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: _checkFirstRun(),
+    return FutureBuilder<String>(
+      future: _getInitialRoute(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
+        if (!snapshot.hasData) {
           return Scaffold(
             body: Center(child: CircularProgressIndicator()),
           );
@@ -27,19 +36,15 @@ class LandingPage extends StatelessWidget {
           );
         } else {
           WidgetsBinding.instance.addPostFrameCallback((_) {
-            if (snapshot.data == true) {
-              Navigator.of(context).pushReplacementNamed('/onboarding');
-            } else {
-              Navigator.of(context).pushReplacementNamed('/homescreen');
-            }
+            Navigator.of(context).pushReplacementNamed(snapshot.data!);
           });
 
+          // Optional: splash image while redirecting
           return Center(
-            child: Image.asset(R.imageOnboarding4, width: 128,),
+            child: Image.asset(R.imageOnboarding4, width: 128),
           );
         }
       },
     );
   }
 }
-
