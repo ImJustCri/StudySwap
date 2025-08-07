1b48ad613c441a7a4865130021422f0ae7423240
ImJustCri <cricirsicily@proton.me>
2025-08-07 13:09:52 +0200

    Update data_provider.dart

--- lib/providers/data_provider.dart
+++ lib/providers/data_provider.dart
@@ -1,18 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:studyswap/providers/user_provider.dart';
 
-final dataProvider = StreamProvider<Map?>((ref) {
-    final userStream = ref.watch(userProvider);
-    var currentUser = userStream.value;
-
-    if (currentUser != null) {
-      var docRef = FirebaseFirestore.instance.collection('Users').doc(currentUser.uid);
-
-      return docRef.snapshots().map((doc) => doc.data());
-    } else {
-      return Stream.empty();
-    }
-  },
-);
+final dataProvider = StreamProvider.family<Map<String, dynamic>?, String>((ref, userId) {
+  final docRef = FirebaseFirestore.instance.collection('Users').doc(userId);
+  return docRef.snapshots().map((doc) => doc.data());
+});
